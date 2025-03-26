258601c9f026a3ea6a08936842555c4d9c15b08f
Felix-Achiah <felixachiah@gmail.com>
2025-03-26 14:41:44 +0000

    db config

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from dotenv import load_dotenv
 from datetime import timedelta
 from pathlib import Path
@@ -142,15 +143,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'URL': os.getenv('DB_URL'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DB_URL'))
 }
 
 
@@ -196,7 +189,7 @@ STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'user_auth.User'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://trip-planner-mu-bice.vercel.app/",
+    "https://trip-planner-mu-bice.vercel.app",
     "http://localhost:5173"
 ]
 
